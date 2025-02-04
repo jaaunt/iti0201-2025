85ffff5ede1203aa2904eb20fe601a62193da3a3
jaaunt <jaaunt@taltech.ee>
2025-02-04 16:56:36 +0200

    hi :)

--- EX01/EX01.py
+++ EX01/EX01.py
@@ -1,3 +1,22 @@
-while (True):
-    print("Hello world!")
-    break
+"""EX01: SPA."""
+
+
+class Robot:
+    """Turtlebot robot."""
+
+    def __init__(self, robot: object) -> None:
+        """Class initializer.
+
+        Args:
+            robot (object): An instance of a Turtlebot-like robot interface.
+        """
+        self.robot = robot
+
+    def sense(self):
+        pass
+
+    def plan(self):
+        pass
+
+    def act(self):
+        pass
