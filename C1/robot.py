5685ab236156495bd3ce3986845bd887a90cbc9f
jaaunt <jaaunt@taltech.ee>
2025-03-28 15:07:03 +0200

    nu uh sus test

--- C1/robot.py
+++ C1/robot.py
@@ -1,65 +1,62 @@
-"""C1."""
+import numpy as np
 
 class Robot:
-    import numpy as np
+    """Turtlebot robot."""
 
-    class Robot:
-        """Turtlebot robot."""
+    def __init__(self, robot: object) -> None:
+        """Class initializer."""
+        self.robot = robot
+        self.found_object = False
+        self.object_distance = 0.0
 
-        def __init__(self, robot: object) -> None:
-            """Class initializer."""
-            self.robot = robot
-            self.found_object = False
-            self.object_distance = 0.0
+    def detect_blue_object(self) -> bool:
+        """Detect a blue object using the robot's camera."""
+        image = self.robot.get_camera_rgb_image()
+        detected = self.process_for_blue(image)
+        return detected
 
-        def detect_blue_object(self) -> bool:
-            """Detect a blue object using the robot's camera."""
-            image = self.robot.get_camera_rgb_image()
-            detected = self.process_for_blue(image)
-            return detected
+    def process_for_blue(self, image) -> bool:
+        """Process the camera image to check for a blue object."""
+        blue_detected = np.any(image[:, :, 0] > 100)
+        return blue_detected
 
-        def process_for_blue(self, image) -> bool:
-            """Process the camera image to check for a blue object."""
-            blue_detected = np.any(image[:, :, 0] > 100)
-            return blue_detected
+    def drive_towards_object(self) -> None:
+        """Drive towards the detected blue object."""
+        self.robot.set_left_motor_velocity(0.5)
+        self.robot.set_right_motor_velocity(0.5)
 
-        def drive_towards_object(self) -> None:
-            """Drive towards the detected blue object."""
-            self.robot.set_left_motor_velocity(0.5)
-            self.robot.set_right_motor_velocity(0.5)
+    def stop_in_front_of_object(self) -> None:
+        """Stop the robot in front of the blue object."""
+        self.robot.set_left_motor_velocity(0)
+        self.robot.set_right_motor_velocity(0)
 
-        def stop_in_front_of_object(self) -> None:
-            """Stop the robot in front of the blue object."""
-            self.robot.set_left_motor_velocity(0)
-            self.robot.set_right_motor_velocity(0)
+    def search_for_object(self) -> None:
+        """Turn the robot to search for the blue object."""
+        self.robot.set_left_motor_velocity(-0.2)
+        self.robot.set_right_motor_velocity(0.2)
 
-        def search_for_object(self) -> None:
-            """Turn the robot to search for the blue object."""
-            self.robot.set_left_motor_velocity(-0.2) # turn turn turn
-            self.robot.set_right_motor_velocity(0.2)
+    def sense(self) -> None:
+        """Gather sensor data."""
+        self.found_object = self.detect_blue_object()
 
-        def sense(self) -> None:
-            """Gather sensor data."""
-            self.found_object = self.detect_blue_object()
-
-        def plan(self) -> None:
-            """Plan the robot's actions."""
-            if self.found_object:
-                self.object_distance = self.robot.get_lidar_range_list()
-                if self.object_distance > 0.5:
-                    self.drive_towards_object()
-                else:
-                    self.stop_in_front_of_object()
+    def plan(self) -> None:
+        """Plan the robot's actions."""
+        if self.found_object:
+            self.object_distance = self.robot.get_lidar_range_list()
+            if self.object_distance > 0.5:
+                self.drive_towards_object()
             else:
-                self.search_for_object()
+                self.stop_in_front_of_object()
+        else:
+            self.search_for_object()
 
-        def act(self) -> None:
-            """Execute the actions."""
-            pass
+    def act(self) -> None:
+        """Execute the actions."""
+        pass
 
-        def spin(self) -> None:
-            """Main loop."""
-            while True:
-                self.sense()
-                self.plan()
-                self.act()
+    def spin(self) -> None:
+        """Main loop."""
+        while True:
+            self.sense()
+            self.plan()
+            self.act()
