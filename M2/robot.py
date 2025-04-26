74d8ceb2f4a6c84c6a05270020d5cf3bbedffd66
jaaunt <jaaunt@taltech.ee>
2025-04-27 01:06:39 +0300

    hi :)

--- M2/robot.py
+++ M2/robot.py
@@ -80,7 +80,7 @@ class Robot:
         # Trükime kõik vajalikud andmed
         print(f"center={self.ir_center:.1f} | left={self.ir_left:.1f} | right={self.ir_right:.1f} | state={self.state} | orientation={math.degrees(self.orientation):.1f}°")
 
-    def is_camera_mostly_black(self, threshold=0.7):
+    def is_camera_mostly_black(self, threshold=0.62):
         """Kontrollib kas enamus kaamerapildist on must."""
         image = self.robot.get_camera_rgb_image()
         # ignoreerime Alpha channeli
