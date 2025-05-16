35e4d55c424a13d33c908c6d96e10f5ad79b50a8
jaaunt <jaaunt@taltech.ee>
2025-05-16 10:37:15 +0300

    nu uh sus test

--- M3/robot.py
+++ M3/robot.py
@@ -341,10 +341,10 @@ class Robot:
 
         self.lidar = self.robot.get_lidar_range_list()
         if self.lidar:
-            self.dir_lidar["front"] = self.lidar[480]  # front (0 degrees)
-            self.dir_lidar["back"] = self.lidar[150]  # back (180 degrees)
-            self.dir_lidar["left"] = self.lidar[320]  # left (90 degrees)
-            self.dir_lidar["right"] = self.lidar[1]  # right (270 degrees)
+            self.dir_lidar["front"] = min(self.lidar[475:485])  # front (0 degrees)
+            self.dir_lidar["back"] = min(self.lidar[145:155])  # back (180 degrees)
+            self.dir_lidar["left"] = min(self.lidar[315:325])  # left (90 degrees)
+            self.dir_lidar["right"] = min(self.lidar[1])  # right (270 degrees)
 
     def plan(self) -> None:
         """Plan the robot's actions.
